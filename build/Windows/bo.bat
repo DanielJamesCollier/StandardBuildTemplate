b305baa2b861296e0ff5079f77637f25f6c266d0
Daniel Collier <daniel.collier@hotmail.co.uk>
2017-02-15 13:52:55 +0000

    standard exe name
    add standard exe name to build "main.exe"
    move subsystem flag to before src file.
    change are the same across optimized build and normal build

--- build/Windows/bo.bat
+++ build/Windows/bo.bat
@@ -3,8 +3,8 @@ echo ------------------------
 echo ------Build Started-----
 echo ------------------------
 echo:
-echo cl /std:c++14 /WX /O2 ../../src/main.cpp /SUBSYSTEM:CONSOLE
-cl /std:c++14 /WX /O2 ../../src/main.cpp /SUBSYSTEM:CONSOLE
+echo cl /std:c++14 /WX /O2 /SUBSYSTEM:CONSOLE /Femain ../../src/main.cpp 
+cl /std:c++14 /WX /O2 /SUBSYSTEM:CONSOLE /Femain ../../src/main.cpp 
 echo ------------------------
 echo:
 echo:
