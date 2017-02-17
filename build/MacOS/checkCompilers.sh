7c4e9773384fa25ef7cf985ac1990df62dcd00d8
Daniel Collier <daniel.collier@hotmail.co.uk>
2017-02-17 10:24:32 +0000

    add setupAllScriptsRunnable.sh & add checkCompilers.sh
    setupAllScriptsRunnable.sh: chmod a+x on all scripts to save time doing
    it manually.
    checkCompilers.sh: calls —version on supported compilers to see if they
    are install.

---
+++ build/MacOS/checkCompilers.sh
@@ -0,0 +1,13 @@
+echo -e "\n\n"
+echo "------------------------"
+echo "-----Check Compilers----"
+echo "------------------------"
+echo "clang"
+echo "---------------"
+clang++ --version
+echo -e "\n"
+echo "gcc"
+echo "---------------"
+gcc --version
+echo "------------------------"
+echo -e "\n\n"
---
+++ build/MacOS/setupAllScriptsRunnable.sh
@@ -0,0 +1,18 @@
+echo -e "\n\n"
+echo "------------------------"
+echo "-----Enable Scripts-----"
+echo "------------------------"
+echo chmod a+x ./checkCompilers.sh
+chmod a+x ./checkCompilers.sh
+echo chmod a+x ./clang++/variables.sh
+chmod a+x ./clang++/variables.sh
+echo chmod a+x ./clang++/c.sh
+chmod a+x ./clang++/c.sh
+echo chmod a+x ./clang++/b.sh
+chmod a+x ./clang++/b.sh
+echo chmod a+x ./clang++/bo.sh
+chmod a+x ./clang++/bo.sh
+echo chmod a+x ./clang++/r.sh
+chmod a+x ./clang++/r.sh
+echo "------------------------"
+echo -e "\n\n"
