3f040c30257603374fa2b325e74037574a9ddaf5
Daniel Collier <daniel.collier@hotmail.co.uk>
2017-02-19 18:59:13 +0000

    update clean and run batch files to new standard

--- build/Windows/cl/c.bat
+++ build/Windows/cl/c.bat
@@ -1,12 +1,16 @@
 @echo off
-echo ------------------------
-echo ------CLEAN STARTED-----
-echo ------------------------
+echo:
+echo:
+echo =======================
+echo =====CLEAN STARTED=====
+echo =======================
 echo:
 @echo on
 del *.obj
 del *.exe
 @echo off
-echo ------------------------
+echo =======================
+echo =====CLEAN FINISHED====
+echo =======================
 echo:
 echo:
--- build/Windows/cl/r.bat
+++ build/Windows/cl/r.bat
@@ -1,11 +1,18 @@
 @echo off
-echo ------------------------
-echo -------RUN STARTED------
-echo ------------------------
-echo:
-main.exe
+
+call variables.bat
+
 echo:
 echo:
-echo ------------------------
+echo =======================
+echo ======RUN STARTED======
+echo =======================
+echo:
+%exeName%
+echo:
+echo:
+echo =======================
+echo ======RUN FINISHED=====
+echo =======================
 echo:
 echo:
